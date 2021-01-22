0bbb82db680bf14f5090b10739ee883a56f6d0ef
Richard Cho <richardcho100@gmail.com>
2021-01-21 21:18:19 -0500

    Assign a specific color for each response

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
 
 const input = process.argv[2]; 
 //the above line grabs and stores the string argument that was entered when this file was run
 const langCode = franc(input);
 
 if (langCode === 'und') {
-  console.log('Sorry cannot identify language. Please try a larger sample of text.')
+  console.log('Sorry cannot identify language. Please try a larger sample of text.'.red)
 } else {
   const language = langs.where('3', langCode);
-  console.log(`This language seems to be ${language.name}.`)
+  console.log(`This language seems to be ${language.name}.`.green)
 }
